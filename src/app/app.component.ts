ea978503c145b87e50e074c37e35cd9edd039060
ckd1618 <ckd1618@gmail.com>
2022-09-26 16:44:10 -0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,16 +10,12 @@ export class AppComponent  {
   lengthOfArr;
   currentBackup;
   index = 0;
-
   greek = ["alpha", "beta", "gamma", "delta", "epsilon", "zeta", "eta", "theta", "iota", "kappa", "lambda", "mu", "nu", "xi", "omicron", "pi", "rho", "sigma", "tau", "upsilon", "phi", "chi", "psi", "omega"];
-  
   armenian = ["ah / a:", "b", "g", "d", "ye", "z", "e / eh", "uh / ə", "t (aspirated)", "zh / 3", "i", "l", "kh / x", "ts (non-aspirated)", "k", "h", "dz", "gh / ŧ", "tch", "m", "y", "n", "sh / ∫", "vo", "ch", "p", "dj", "r (strong)", "s", "v", "t", "r (soft)", "ts (aspirated)", "oo", "p (aspirated)", "q (aspirated)", "yev", "o", "f"];
-  
   backupArrGreek = [...this.greek];
   backupArrArmenian = [...this.armenian];
-
   
-
+  
   constructor() {
     this.chooseLang(this.backupArrGreek);
   }
